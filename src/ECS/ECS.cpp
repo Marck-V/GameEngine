1e16f15fae9e39d4e2d2cd416e98a1ddd3ebbacf
Marck-V <42965997+Marck-V@users.noreply.github.com>
2023-11-11 18:26:20 -0800

    added code to the manager and system classes.

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,7 +1,44 @@
 #include "ECS.h"
+#include <spdlog/spdlog.h>
 
 int Entity::GetID() const
 {
 	return id;
 	
+}
+
+void System::AddEntity(Entity entity)
+{
+	entities.push_back(entity);
+}
+
+void System::RemoveEntity(Entity entity)
+{
+	// Modern C++ way of iterating through a vector and removing an element. Using a lambda function.
+	entities.erase(std::remove_if(entities.begin(), entities.end(), [&entity](Entity e) {return e == entity; }), entities.end());
+}
+
+std::vector<Entity> System::GetSystemEntities() const
+{
+	return entities;
+}
+
+const Signature& System::GetComponentSignature() const
+{
+	// TODO: insert return statement here
+	return componentSignature;
+}
+
+Entity Manager::CreateEntity()
+{
+	int entityID = 0;
+	
+	entityID = numEntities++;
+
+	Entity entity(entityID);
+	entitiesToCreate.insert(entity);
+
+	spdlog::info("Entity created with ID: {0}", entityID);
+
+	return entity;
 }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -2,6 +2,10 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
+#include <set>
+
 const unsigned int MAX_COMPONENTS = 32;
 
 typedef std::bitset<MAX_COMPONENTS> Signature;
@@ -22,6 +26,12 @@ public:
 		Entity(int id) : id(id) {};
 
 		int GetID() const;
+
+		Entity& operator=(const Entity& other) = default;
+			
+		bool operator==(const Entity& other) const {
+			return id == other.id;
+		}
 		
 };
 
@@ -38,14 +48,123 @@ public:
 	void AddEntity(Entity entity);
 	void RemoveEntity(Entity entity);
 
-	std::vector<Entity> GetEntities() const;
-	Signature& GetComponentSignature() const;
+	std::vector<Entity> GetSystemEntities() const;
+
+	const Signature& GetComponentSignature() const;
+
+	// Defines the component type that entities must have to be added to the system
+	template <typename T> void RequireComponent();
 };
 
-class Component {
+struct IComponent {
+	protected:
+		static int nextID;
+};
+
+// Used to assign a unique IDS to each component type.
+template<typename T> 
+class Component : public IComponent{
+	// Returns the unique ID for the component type.
+	static int GetID(){
+		static auto id = nextID++;
+		return id;
+	}
+		
+};
+
+template <typename T> void System::RequireComponent() {
+	const auto componentID = Component<T>::GetID();
+	componentSignature.set(componentID);
+}
+
+
+class IPool {
+public:
+	virtual ~IPool() {};
 
 };
 
+
+template <typename T>
+class Pool : public IPool {
+private:
+	std::vector<T> data;
+
+public:
+	Pool(int size = 100) {
+		data.resize(size);
+	}
+	virtual ~Pool() = default;
+
+	bool isEmpty() const {
+		return data.empty();
+	}
+
+	int GetSize() const {
+		return data.size();
+	}
+
+	void Resize(int size) {
+		data.resize(size);
+	}
+
+	void Clear() {
+		data.clear();
+	}
+
+	void Add(T object) {
+		data.push_back(component);
+	}
+
+
+	void Set(int entityID, T object) {
+		data[index] = component;
+	}
+
+	T& Get(int entityID) {
+		return static_cast<T&>(data[index]);
+	}
+
+	T& operator [](unsigned int index) {
+		return data[index];
+	}
+
+};
+
+
+// The manager is going to be the class that handles the creation and destruction of entities and components.
 class Manager {
+private:
+	int numEntities = 0;
 
+	// A vector of componenet pools. Each pool is going to comntain the components of a specific type.
+	// The index of the pool in the vector is going to be the ID of the component type.
+	// This way we can access the pool of a specific component type by using the ID of the component type.
+	std::vector<IPool*> componentPools;
+
+	// A vector of signatures. Each signature is going to represent the component types that an entity has.
+	// The index of the signature in the vector is going to be the ID of the entity.
+	std::vector<Signature> entityComponentSignatures;
+
+	// An unordered map of systems.
+	std::unordered_map<std::type_index, System*> systems;
+
+	// Set of entities that are going to be added or destroyed at the end of the frame.
+	std::set<Entity> entitiesToDestroy;
+	std::set<Entity> entitiesToCreate;
+
+	
+	
+public:
+
+	Manager() = default;
+
+	void Update();
+
+	Entity CreateEntity();
+	
+	void AddEntityToSystem(Entity entity, System* system);
+	
+
+	
 };
