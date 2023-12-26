7fc92f1d5e39ee280a0bbf9fb50698bb3f9c1763
Marck-V <42965997+Marck-V@users.noreply.github.com>
2023-12-26 11:11:33 -0800

    Starting to use the sol library to be able to control out gam engine with lua scripting.

---
+++ assets/scripts/test.lua
@@ -0,0 +1,22 @@
+-- This is lua global variable
+some_variable = 7 * 6
+
+-- This is a lua table.
+config = {
+	title = "Game Engine",
+	fullscreen = false,
+	resolution = {
+		width = 800,
+		height = 600
+	}
+}
+
+function factorial(n)
+	if n == 0 then
+		return 1
+	else
+		return n * factorial(n - 1)
+	end
+end
+
+print(factorial(7))
